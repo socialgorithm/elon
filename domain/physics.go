db9f0d76ab121bf385c64e09523f9b36ec2fe3aa
Alex Millward <millwarda@googlemail.com>
2018-05-28 16:04:59 +0100

    #1 Added domain structures

---
+++ domain/physics.go
@@ -0,0 +1,26 @@
+package domain
+
+// CarControlState represents a command to alter the behaviour of a car
+type CarControlState struct {
+	Throttle float32
+	Steering float32
+}
+
+// CarState represents the current reportable state of a car
+type CarState struct {
+	Position Position
+	Velocity float32
+	Sensors  []Sensor
+}
+
+// Position represents an (X, Y) position
+type Position struct {
+	X float32
+	Y float32
+}
+
+// Sensor represents the current state of a sensor
+type Sensor struct {
+	Angle    int8
+	Distance float32
+}
