3cbfd1c74e549e68d222f4f95aadae4d0b0418a9
Alejandro U. Alvarez <aluralma@gmail.com>
2018-05-29 18:29:55 +0100

    modify render to take car channel and track

--- track/constants.go
+++ track/constants.go
@@ -1,7 +1,8 @@
 package track
 
-const points = 50
+const minPoints = 10
+const maxPoints = 50
 const difficulty = float64(0.2) // closer to 0 will create sharper turns, exponentially
 const maxDisplacement = float64(200)
-const margin = float64(100)
+const margin = float64(50)
 const roadWidth = float64(25)
