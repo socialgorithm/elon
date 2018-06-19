fcf30578efd31c1600522a3ba077ac02dcde0115
Alejandro U. Alvarez <aluralma@gmail.com>
2018-06-19 14:41:31 +0100

    wheel turning - wrong matrix for now

--- render/constants.go
+++ render/constants.go
@@ -1,14 +1,19 @@
 package render
 
-import "golang.org/x/image/colornames"
+import (
+	"math"
+
+	"golang.org/x/image/colornames"
+)
 
 const lineThickness = 4
 const segmentLength = 15
 const carWidth = 15
 const carLength = 30
-const wheelOffset = 5
-const wheelLength = 5
+const wheelOffset = carLength / 7
+const wheelLength = carLength / 4
 const wheelWidth = 3
+const maxWheelSteering = math.Pi / 4
 
 var bgColor = colornames.Green
 var roadColor = colornames.Gray
--- render/render.go
+++ render/render.go
@@ -44,9 +44,18 @@ func run() {
 	)
 
 	trackRender := renderTrack(simulation.Track)
+	camZoom := 1.8
 
 	for !win.Closed() {
 		win.Clear(bgColor)
+
+		// follow top car with camera and zoom
+		camPos := pixel.V(
+			cars[0].CarState.Position.X,
+			cars[0].CarState.Position.Y,
+		)
+		cam := pixel.IM.Scaled(camPos, camZoom).Moved(win.Bounds().Center().Sub(camPos))
+		win.SetMatrix(cam)
 
 		// redraw the track
 		trackRender.Draw(win)
@@ -54,7 +63,7 @@ func run() {
 		// update cars
 		if len(cars) > 0 {
 			for i := range cars {
-				carRender := renderCar(cars[i].CarState)
+				carRender := renderCar(cars[i])
 				carRender.Draw(win)
 			}
 		}
